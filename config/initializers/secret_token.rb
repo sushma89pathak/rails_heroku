a027731ec3314c6e2e10155bcb5f8a9b871241d2
sushma_pathak <sushma@railsfactory.org>
2016-10-10 00:45:44 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '24be42110e48e99647d1ae122419cdd99e6f4ce2e3c6465b6401a5184de0a348dd9e154c8c2e0d8af0c0865aa1a47531124c3903c41ffbc89cb5960f37c22b33'
